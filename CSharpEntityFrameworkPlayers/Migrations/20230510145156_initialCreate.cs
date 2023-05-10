5e373470199ec9860889c1d3d0522fb3cec6ddbc
thrudr <ariel.oliveiraesilva@gmail.com>
2023-05-10 16:56:00 +0200

    first-migration

---
+++ CSharpEntityFrameworkPlayers/Migrations/20230510145156_initialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSharpEntityFrameworkPlayers.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    cognome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    punteggio = table.Column<int>(type: "int", nullable: false),
+                    numeroPartiteGiocate = table.Column<int>(type: "int", nullable: false),
+                    numeroPartiteVinte = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
