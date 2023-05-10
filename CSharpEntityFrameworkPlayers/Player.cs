8887807223259985d24993987558175f087db3f9
thrudr <ariel.oliveiraesilva@gmail.com>
2023-05-10 16:04:09 +0200

    player-class-attributes

---
+++ CSharpEntityFrameworkPlayers/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEntityFrameworkPlayers
+{
+    public class Player
+    {
+        //ATTRIBUTI
+        public int id;
+        public string nome;
+        public string cognome;
+        public int punteggio;
+        public int numeroPartiteGiocate;
+        public int numeroPartiteVinte;
+
+
+    }
+}
